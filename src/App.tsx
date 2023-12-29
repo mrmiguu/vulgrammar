2e178119c609e3c31af687266d6c5aee3f57e791
Alex <a.matulionis@usa.com>
2023-12-28 20:48:12 -0800

    Make words addable and removable

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, useMemo } from 'react'
+import { PropsWithChildren, useMemo, useState } from 'react'
 
 import { IlluminatedLetterAssetId, useIlluminatedLetterImageAsset } from './assets.illuminatedLetters'
 import bibleVulgate from './bible.vulgate'
@@ -7,7 +7,13 @@ import { abs, keys, pickRandom, shuffle } from './utils'
 function Word({ className, onClick, children }: PropsWithChildren<{ className?: string; onClick?: () => void }>) {
   const cls = `inline-block mx-1 my-1 p-0.5 font-serif text-2xl align-bottom rounded leading-none ${className}`
 
-  return onClick ? <button className={cls}>{children}</button> : <div className={cls}>{children}</div>
+  return onClick ? (
+    <button className={cls} onClick={onClick}>
+      {children}
+    </button>
+  ) : (
+    <div className={cls}>{children}</div>
+  )
 }
 
 function WordIlluminated({ className, children: word }: { className: string; children: string }) {
@@ -66,7 +72,7 @@ function App() {
   // const verse = bibleVulgate['Gen']['1']['22'] // B
   // const verse = bibleVulgate['Gen']['1']['21'] // C
   // const verse = bibleVulgate['Gen']['1']['3'] // D
-  const verse = bibleVulgate['Jde']['1']['24'] // E - average
+  const verse = bibleVulgate['Jde']['1']['24'] // E - longest subverse
   // const verse = bibleVulgate['Gen']['1']['16'] // F
   // const verse = bibleVulgate['Gen']['4']['20'] // G
   // const verse = bibleVulgate['Gen']['5']['1'] // H
@@ -102,21 +108,46 @@ function App() {
   }
 
   const subverseWords = verseWords.slice(0, punctuationIndexClosestTo17 + 1 || undefined)
-  const subverseWordsShuffled = shuffle([...subverseWords], { seed })
+
+  const subverseWordsShuffled = useMemo(
+    () => shuffle([...subverseWords], { seed }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [seed],
+  )
+
+  const [subverseWordBuffer, setSubverseWordBuffer] = useState<string[]>([subverseWords[0]])
 
   return (
     <div className="absolute w-full h-full">
       <div className="fixed w-full h-full bg-yellow-50" />
 
       <div className="absolute flex justify-center w-full h-full">
-        <div className="p-1 text-justify max-w-96">
-          <WordIlluminated className="text-gray-700 border border-yellow-400">{subverseWords[0]}</WordIlluminated>
-          {subverseWords.slice(1).map((word, i) => (
-            <Word key={i} className="text-gray-700 border border-yellow-400">
-              {word}
-            </Word>
-          ))}
-          <Word className="px-2 font-mono text-red-500 border border-red-500 shadow-inner">↩︎</Word>
+        <div className="p-1 text-left w-96">
+          {subverseWordBuffer.length > 0 && (
+            <>
+              <WordIlluminated className="text-gray-700 border border-yellow-400">
+                {subverseWordBuffer[0]}
+              </WordIlluminated>
+              {subverseWordBuffer.slice(1).map((word, i) => (
+                <Word key={i} className="text-gray-700 border border-yellow-400">
+                  {word}
+                </Word>
+              ))}
+
+              {subverseWordBuffer.length > 1 && (
+                <Word
+                  className="px-2 font-mono text-red-500 border border-red-500 shadow-inner"
+                  onClick={() => {
+                    if (subverseWordBuffer.length > 1) {
+                      setSubverseWordBuffer(subverseWordBuffer.slice(0, -1))
+                    }
+                  }}
+                >
+                  ↩︎
+                </Word>
+              )}
+            </>
+          )}
         </div>
       </div>
 
@@ -126,6 +157,9 @@ function App() {
             <Word
               key={i}
               className="px-2 py-3 text-xl text-gray-700 bg-yellow-50 rounded-md shadow-[0_1px_1px_0_rgba(0,0,0,0.4)]"
+              onClick={() => {
+                setSubverseWordBuffer([...subverseWordBuffer, word])
+              }}
             >
               {word.replace(punctuationRegex, '').toLowerCase()}
             </Word>
